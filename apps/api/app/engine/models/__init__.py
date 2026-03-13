da96d35e23fb1faea67a9d89177b89a477561f59
Richard Rivera Roman <richard.rivera.roman+github@gmail.com>
2026-03-13 02:59:52 -0400

    feat(core): export Property, Railroad, and Utility from models package

--- apps/api/app/engine/models/__init__.py
+++ apps/api/app/engine/models/__init__.py
@@ -2,7 +2,20 @@
 
 from .board import Board
 from .player import Player
+from .property import Property
+from .railroad import Railroad
 from .space import Space, SpaceColor, SpaceType
 from .title_deed import TitleDeed
+from .utility import Utility
 
-__all__ = ["Board", "Player", "Space", "SpaceColor", "SpaceType", "TitleDeed"]
+__all__ = [
+    "Board",
+    "Player",
+    "Property",
+    "Railroad",
+    "Space",
+    "SpaceColor",
+    "SpaceType",
+    "TitleDeed",
+    "Utility",
+]
